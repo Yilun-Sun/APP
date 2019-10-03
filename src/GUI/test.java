dddf08af24be329948795c314d0a43799c05fe44
Yilun-Sun <osirisnero@gmail.com>
2019-10-02 20:29:39 -0400

    change

--- src/GUI/test.java
+++ src/GUI/test.java
@@ -36,7 +36,7 @@ public class test {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
